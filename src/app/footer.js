9b774e333d68d4a1a8b3a73d538cb6fd8b79206f
Himanshu Vashist <35025815+himanshuvashist@users.noreply.github.com>
2025-09-05 23:40:11 +0530

    starting

---
+++ src/app/footer.js
@@ -0,0 +1,7 @@
+const Foot = () => {
+    return <section>
+        <div className="min-h-[100svh] bg-blue-50"><p>hi</p></div>
+    </section>
+}
+
+export default Foot;
--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,196 @@
+"use client";
+import {useEffect, useRef, useState} from "react";
+import LocomotiveScroll from 'locomotive-scroll';
 import Image from "next/image";
+import Footer from "@/app/footer";
+import Foot from "@/app/footer";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    const scrollRef = useRef(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+    useEffect(() => {
+        if (!scrollRef.current) return;
+        const locomotiveScroll = new LocomotiveScroll({
+            el: scrollRef.current,
+            smooth: true,
+            lenisOptions: {
+                lerp: 0.2,
+                duration: 1.3,
+                orientation: "vertical",
+                smoothWheel: true,
+                smoothTouch: true,
+                wheelMultiplier: 1,
+                touchMultiplier: 2,
+            },
+        });
+
+        return () => {
+            locomotiveScroll.destroy();
+        };
+    }, []);
+    return (
+        <div ref={scrollRef}>
+            <div className="flex flex-col h-svh bg-white">
+                <nav id="nav"
+                     className="z-10 flex h-16 justify-between items-center rounded-xl mx-4 fixed top-2 bg-white/60 backdrop-blur-md  left-0 right-0
+                {/*shadow-[inset_-5px_-5px_5px_0px_rgba(255,255,255,.03),inset_5px_4px_5px_0px_rgba(0,0,0,.04)]*/}
+                                 ">
+                    <div>
+                        <p className="p-3 text-black">LOGO</p>
+                    </div>
+                    <div>
+                        <ul className="flex flex-wrap gap-8 px-8 py-4 items-center">
+                            <li><p className="text-black">Technology</p></li>
+                            <li><p className="text-black">Specs</p></li>
+                            <li><p className="text-black">Coming Soon</p></li>
+                            <li><p className="text-black">=</p></li>
+                        </ul>
+                    </div>
+                </nav>
+                <div className="flex-1 bg-amber-200 mx-2 rounded-md m-4 mt-18 relative overflow-hidden">
+                    <video
+                        className="absolute inset-0 w-full h-full object-cover"
+                        src="./video.mp4"
+                        autoPlay
+                        muted
+                        loop
+                        playsInline
+                        aria-hidden="true"
+                    />
+                    <div
+                        className="absolute left-0 bottom-0 flex flex-col bg-black/40 items-center justify-center w-full h-full">
+                    </div>
+                    <div
+                        className="absolute left-0 bottom-0 right-0 flex flex-col  justify-center p-4">
+                        <div className="">
+                            <h3 className="text-white text-7xl font-bold">Command</h3>
+                        </div>
+                        <div className="mt-8">
+                            <h2 className="text-white text-7xl font-bold">Coordinate</h2>
+                        </div>
+                        <div className="mt-4">
+                            <h1 className="text-white text-9xl font-bold">Dominate</h1>
+                        </div>
+                        <hr className="border-t border-gray-300"/>
+                        <div className="flex flex-row justify-between mb-8 mt-4">
+                            <div>
+                                <p className="text-white text-lg">Indian autonomous design propspurs here</p>
+                            </div>
+                            <div>
+                                <p className="text-white text-lg"> Scroll down to view more
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="min-h-[50svh] px-8 pt-8 bg-white flex items-center justify-center">
+                <div>
+                    <div>
+                        <img src="/sectionTwoText.svg" alt="icon" className="w-full"
+                             data-scroll="" data-scroll-speed=".02"
+                        />
+                    </div>
+                    <div className="bg-white pt-4">
+                        <p className="text-9xl text-black text-center"
+                           data-scroll="" data-scroll-speed=".05"
+                        > 2.0 </p>
+                    </div>
+                </div>
+            </div>
+            <div className="px-4 pb-4 bg-white flex flex-row">
+                <div className="flex gap-8 items-center justify-center flex-col"
+                >
+                    <div className="flex gap-4 items-stretch">
+                        <div className="flex flex-1">
+                            <p className="text-7xl text-gray-300">HEAVY DUTY METAL</p>
+                        </div>
+                        <div className="flex flex-2 rounded-2xl bg-white overflow-hidden"
+                             data-scroll="" data-scroll-speed=".01"
+                        >
+                            <img src="https://images.pexels.com/photos/336232/pexels-photo-336232.jpeg"
+                                 className="object-cover  w-full h-full"
+                            />
+                        </div>
+                        <div className="flex items-center flex-1"
+                             data-scroll="" data-scroll-speed=".06"
+                        >
+                            <div className="pt-4">
+                                <p className="text-black text-md font-bold underline">Latest Swamp
+                                    technology</p>
+                                <br/>
+                                <p className="text-gray-500 text-xl tracking-wider">with intigrated cirbuit and long
+                                    duration flight
+                                    time and 2000meter radar system.
+                                    with intigrated cirbuit and long duration flight
+                                    time and 2000meter radar system.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex gap-4 items-stretch">
+                        <div className="flex items-center flex-1"
+                        >
+                            <div className="pt-4">
+                                <p className="text-black text-md font-bold underline">Latest Swamp
+                                    technology</p>
+                                <br/>
+                                <p className="text-gray-500 text-xl tracking-wider">with intigrated cirbuit and long
+                                    duration
+                                    flight
+                                    time and 2000meter radar system.
+                                    with intigrated cirbuit and long duration flight
+                                    time
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-2 rounded-2xl bg-white overflow-hidden"
+                             data-scroll="" data-scroll-speed=".01"
+                        >
+                            <img src="https://images.pexels.com/photos/1170064/pexels-photo-1170064.jpeg"
+                                 className="object-cover  w-full h-full"
+                            />
+                        </div>
+                        <div className="flex flex-1"
+                             data-scroll="" data-scroll-speed=".06"
+                        >
+                            <p className="text-7xl text-gray-300">ALWAYS ON RADAR</p>
+                        </div>
+                    </div>
+                    <div className="flex gap-4 items-stretch"
+                    >
+                        <div className="flex flex-1">
+                            <p className="text-7xl text-gray-300">LONG RANGE</p>
+                        </div>
+                        <div className="flex flex-2 rounded-2xl bg-white overflow-hidden"
+                             data-scroll="" data-scroll-speed=".01"
+                        >
+                            <img src="https://images.pexels.com/photos/2044044/pexels-photo-2044044.jpeg"
+                                 className="object-cover  w-full h-full"
+                            />
+                        </div>
+                        <div className="flex items-center flex-1 "
+                             data-scroll="" data-scroll-speed=".06"
+                        >
+                            <div className="pt-4">
+                                <p className="text-black text-md font-bold underline">Latest Swamp
+                                    technology</p>
+                                <br/>
+                                <p className="text-gray-500 text-xl tracking-wide">with intigrated cirbuit and long
+                                    duration
+                                    flight
+                                    time and 2000meter radar system.
+                                    with intigrated cirbuit and long duration flight
+                                    time and 2000meter radar system.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="min-h-[100svh] bg-amber-100"><p>hi</p></div>
+            <Foot/>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
